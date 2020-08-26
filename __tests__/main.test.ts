a4e7168d2ecef517f9366bc9cf2296234b784938
Ryan Anderson <ryandy@google.com>
2020-08-26 11:36:55 -0400

    .

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -7,12 +7,12 @@ import {getAppToken} from '../src/app_token'
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_GITHUB_APP_ID'] = '123'
-  process.env['INPUT_GITHUB_APP_PEM'] = 'fake-pem'
-  process.env['GITHUB_REPOSITORY'] = 'ryandy/action-app-test'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
-  const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
+//  process.env['INPUT_GITHUB_APP_ID'] = '123'
+//  process.env['INPUT_GITHUB_APP_PEM'] = 'fake-pem'
+//  process.env['GITHUB_REPOSITORY'] = 'ryandy/action-app-test'
+//  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+//  const options: cp.ExecSyncOptions = {
+//    env: process.env
+//  }
+//  console.log(cp.execSync(`node ${ip}`, options).toString())
 })
