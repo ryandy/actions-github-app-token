adb9151b60d755cc4a6764581d9d3109aaf230df
Ryan Anderson <ryandy@google.com>
2020-08-28 11:29:03 -0400

    add logging

--- src/app_token.ts
+++ src/app_token.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core'
 import {createAppAuth} from '@octokit/auth-app'
 import {Octokit} from '@octokit/rest'
 
@@ -9,6 +10,7 @@ export async function getAppToken(appId: string,
 
   // Using the App ID and private key, create an App auth.
   // See https://octokit.github.io/rest.js/v18#authentication
+  core.info('Create App auth')
   const appOctokit: Octokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
@@ -19,6 +21,7 @@ export async function getAppToken(appId: string,
 
   // Using the App auth and repo name, obtain the App installation ID.
   // See https://docs.github.com/en/rest/reference/apps
+  core.info('Get App installation ID')
   const installationResponse = await appOctokit.request(
     `/repos/${repo}/installation`
   )
@@ -26,6 +29,7 @@ export async function getAppToken(appId: string,
 
   // Get the current repo's ID.
   // See https://docs.github.com/en/rest/reference/repos
+  core.info('Get repo ID')
   const defaultOctokit: Octokit = new Octokit()
   const repoResponse = await defaultOctokit.request(`/repos/${repo}`)
   const repoId: number = repoResponse.data['id']
@@ -33,6 +37,7 @@ export async function getAppToken(appId: string,
   // Finally, use the App auth and installation ID to obtain an App installation access token
   // with access limited to the current repo.
   // See https://octokit.github.io/rest.js/v18#apps
+  core.info('Create App installation token')
   const tokenResponse = await appOctokit.apps.createInstallationAccessToken({
     installation_id: installationId,
     repository_ids: [repoId]
